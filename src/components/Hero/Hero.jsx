7f91f8e265fc28c529c8680d97059494907662bb
Rene Cruz <renecruz.0711@gmail.com>
2024-02-09 23:24:19 -0600

    Adding classnames to html tags

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,16 +5,16 @@ import styles from "./Hero.module.css"
 
 const Hero = () => {
   return (
-    <section>
-      <div>
-        <h1>Hi, I am René Cruz</h1>
-        <p>
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I am René Cruz</h1>
+        <p className={styles.description}>
           I am a full-stack developer with 2 years of experience using React and
           Nodejs. Reach out if you would like to learn more!
         </p>
-        <a href="mailto:renecruz.0711@gmail.com">Contact Me!</a>
+        <a href="mailto:renecruz.0711@gmail.com" className={styles.contactBtn}>Contact Me!</a>
       </div>
-      <img src={getImageUrl("hero/heroImage.png")} alt="Hero image of me" />
+      <img className={styles.heroImg} src={getImageUrl("hero/heroImage.png")} alt="Hero image of me" />
       <div className={styles.topBlur}></div>
       <div className={styles.bottomBlur}></div>
     </section>
