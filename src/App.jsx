ba82131efa41c1044be89e6ac9731626028380a0
Rene Cruz <renecruz.0711@gmail.com>
2024-02-15 23:30:59 -0600

    Adding new elements > Experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import styles from "./App.module.css";
 import About from "./components/About/About";
+import Experience from "./components/Experience/Experience";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 
@@ -10,6 +11,7 @@ function App() {
         <Navbar />
         <Hero />
         <About />
+        <Experience />
       </div>
     </>
   );
