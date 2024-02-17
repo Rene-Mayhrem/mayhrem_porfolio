a23bf09f11c9fc3c78641532a0f58164a48df3fe
Rene Cruz <renecruz.0711@gmail.com>
2024-02-16 22:59:06 -0600

    Adding JSON references to the HTML code

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React from "react";
+import skills from "../../data/skills.json";
+import history from "../../data/history.json";
+import { getImageUrl } from "../../utils";
 
 const Experience = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="" id="experience">
+      <h2>Experience</h2>
+      <div className="">
+        <div className="">
+          {skills.map((skill, id) => {
+            return (
+              <div key={id}>
+                <div>
+                  <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <ul className=""></ul>
+      </div>
+    </section>
+  );
+};
 
-export default Experience
+export default Experience;
