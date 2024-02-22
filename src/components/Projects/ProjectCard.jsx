adf4664589cdddceb57f87ebc02d89fa771ffbf4
Rene Cruz <renecruz.0711@gmail.com>
2024-02-21 21:26:25 -0600

    Adding styles to the HTML elements > Card

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -4,20 +4,24 @@ import { getImageUrl } from '../../utils'
 
 const ProjectCard = ({ project: { title, imageSrc, description, skills, demo, source } }) => {
   return (
-    <div>
-      <img src={getImageUrl(imageSrc)} alt={`Image of ${title}`} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-      <ul>
+    <div className={styles.container}>
+      <img
+        className={styles.image}
+        src={getImageUrl(imageSrc)}
+        alt={`Image of ${title}`}
+      />
+      <h3 className={styles.title}>{title}</h3>
+      <p className={styles.description}>{description}</p>
+      <ul className={styles.skills}>
         {
           skills.map((skill, id) => {
-            return <li key={id}>{skill}</li>
+            return <li className={styles.skill} key={id}>{skill}</li>
           })
         }
       </ul>
-      <div>
-        <a href={demo}>demo</a>
-        <a href={source}>source</a>
+      <div className={styles.links}>
+        <a className={styles.link} href={demo}>demo</a>
+        <a className={styles.link} href={source}>source</a>
       </div>
     </div>
   )
