ca200651b6e25453810f4aa17c820b4a17e75a0e
Rene Cruz <renecruz.0711@gmail.com>
2024-02-26 21:28:44 -0600

    Adding an if for menu

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,10 +1,12 @@
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import styles from './ProjectCard.module.css';
 import { getImageUrl } from '../../utils'
 
 const ProjectCard = ({ project: { title, imageSrc, description, skills, demo, source } }) => {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} id='projects'>
       <img
         className={styles.image}
         src={getImageUrl(imageSrc)}
